19c6563ae3a5252fda4be9441ef4d43378ac1227
Pararaum <pararaum@gmail.com>
2017-07-13 16:42:48 +0200

    ⇝ Concurrency

--- course.go
+++ course.go
@@ -4,23 +4,9 @@ import (
 	"fmt"
 )
 
-type nType complex128
-
-func (n nType) foo() int {
-	return 11
-}
-
-func (n nType) bar() int {
-	return 22
-}
-
-func (n nType) snafu() int {
-	return 33
-}
-
 func main() {
-	var fn func(nType) int
-	fn = nType.snafu
-	var y nType
-	fmt.Println(fn(y))
+	go func() {
+		fmt.Println("~~~")
+	}()
+	//No output!
 }
