4b62b70280977e66dc414c96a5d059ab2e7b74b4
Pararaum <pararaum@gmail.com>
2017-07-13 15:39:32 +0200

    Embedding interfaces in types.

--- course.go
+++ course.go
@@ -2,53 +2,17 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"os"
 )
 
-func print(x interface{}) {
-	switch t := x.(type) {
-	case int:
-		fmt.Println("int", x)
-	case float32:
-		fmt.Println("int", x)
-	case string:
-		fmt.Println("string", x)
-	default:
-		fmt.Printf("unknown type `%s`: '%s'\n", t, x)
-	}
+type counter struct {
+	io.Reader
+	n int
 }
 
-//Interface definition.
-type vehicle interface {
-	//Only methods!
-	start()
-	stop()
-}
-
-type car int
-
-func (c car) start() {
-}
-
-func (c car) stop() {
-}
-
-//Now magically car implements the interface of vehicle as it has all
-//the functions...
-
-func do(v vehicle) {
-}
-
-//Does not wirk with nil
-type something struct{}
 
 func main() {
-	var c car
-
-	fmt.Println(c)
-	do(c)
-
-	//Multitype, variadic functions
-	print(1)
-	print(1.1111)
-	print("Hello")
+	fmt.Println("␌")
+	io.Copy(os.Stdout, os.Stdin)
 }
