0532a03c45ffcd249fa472742e2b592ea2ddd4b2
Pararaum <pararaum@gmail.com>
2017-07-13 12:21:06 +0200

    ⇝Strings and maps

--- course.go
+++ course.go
@@ -8,7 +8,14 @@ func main() {
 	x := []byte{1, 23}
 	y := []byte{1, 23}
 	h := make(map[string]int)
-	h[string(x)]++
+	/*
+		h[x]++
+
+		# github.com/Edward-Morbius/go_course
+		./course.go:11: cannot use x (type []byte) as type string in map index
+
+	*/
+
 	h[string(y)]++
 	fmt.Println(h)
 }
