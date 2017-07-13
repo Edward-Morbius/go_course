1e1c9986fdf23f093be7a22c1826f12651b9113a
Pararaum <pararaum@gmail.com>
2017-07-13 16:55:32 +0200

    Range in channel.

--- course.go
+++ course.go
@@ -9,9 +9,13 @@ func main() {
 	//Capacity of channel is two: ch := make(chan int, 2)
 	ch := make(chan int)
 	go func() {
-		x := <- ch
-		fmt.Println("~~~", x)
+		// No index for channel ranges.
+		for x := range ch {
+			fmt.Println("~~~", x)
+		}
 	}()
 	ch <- 100
+	ch <- 101
+	ch <- 102
 	time.Sleep(time.Second)
 }
