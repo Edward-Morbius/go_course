207e368d998ffc5bbc6dc1880a1d8d35362caf16
Pararaum <pararaum@gmail.com>
2017-07-13 14:09:01 +0200

    Inheritance.

--- course.go
+++ course.go
@@ -4,37 +4,19 @@ import (
 	"fmt"
 )
 
-//myType is based on int
-type myType int
+type base int
 
-//myType2 is based on myType is based on int!!!
-type myType2 myType
-
-type Never struct{}
-
-//Remember that mt is BY VALUE!
-//    ↓↓↓↓↓↓↓↓↓↓ Receiver
-func (mt myType) print() {
-	fmt.Println(mt)
+type sub struct {
+	base
 }
 
-//    ↓↓↓↓↓↓↓↓↓↓↓ Pointer receiver.
-func (mt *myType) modify() {
-	if mt != nil {
-		*mt = 42
-	}
+func (b base) print() {
+	fmt.Println("base", b)	
 }
 
 func main() {
-	var x myType
-	fmt.Println(myType(32) < 53)
-	x = 99
-	x.print()
-	x.modify()
-	x.print()
-	var y *myType
-	//This works!
-	y.modify()
-	//This fails!
-	y.print()
+	var b base = 9
+	b.print()
+	var s sub
+	s.print()
 }
