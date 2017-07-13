f4e68752330dd67d74ec937eb43afe026dff8590
Pararaum <pararaum@gmail.com>
2017-07-13 11:57:57 +0200

    Now using range, range uses utf-8.

--- course.go
+++ course.go
@@ -4,10 +4,13 @@ import (
 	"fmt"
 )
 
-
 func main() {
 	s := "H⇝ello"
 	fmt.Println(s)
 	fmt.Printf("%c\n", s[1])
 	fmt.Println(len(s))
+	//range assumes it is UTF-8!
+	for i, c := range s {
+		fmt.Printf("%2d: '%c'\n", i, c)
+	}
 }
