5ac037b9bfa7d2cb5e33141e20db36bd87cab280
Pararaum <pararaum@gmail.com>
2017-07-13 14:11:21 +0200

    Inheritance.

--- course.go
+++ course.go
@@ -14,6 +14,11 @@ func (b base) print() {
 	fmt.Println("base", b)	
 }
 
+//Overload for derived type
+func (s sub) print() {
+	fmt.Println("sub", s)
+}
+
 func main() {
 	var b base = 9
 	b.print()
