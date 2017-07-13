417de69d1a727188061ad3ea3080350583645c37
Pararaum <pararaum@gmail.com>
2017-07-13 11:53:30 +0200

    ⇝ Strings

--- course.go
+++ course.go
@@ -4,26 +4,9 @@ import (
 	"fmt"
 )
 
-func deepNested() int {
-	panic("ERROR")
-	fmt.Println("Nested")
-	return 42
-}
-
-func intensiveAnalysis() (y int, err error) {
-	defer func() {
-		if x:= recover(); x != nil {
-			err = fmt.Errorf("Failed %#v", x)
-		}
-	}()
-
-	//
-	x := deepNested()
-	return x, nil
-}
-
 
 func main() {
-	val, err := intensiveAnalysis()
-	fmt.Println(val, err)
+	s := "Hello"
+	fmt.Println(s)
+	fmt.Printf("%c\n", s[1])
 }
