21bdebaee294177bf5e2d0999706c4845f9ece6d
Pararaum <pararaum@gmail.com>
2017-07-13 15:53:24 +0200

    Extending interfaces, counting bytes.

--- course.go
+++ course.go
@@ -11,8 +11,16 @@ type counter struct {
 	n int
 }
 
+func (c *counter) Read(b []byte) (int, error) {
+	n, err := c.Reader.Read(b)
+	c.n += n
+	//Read reads n bytes into b and return the number of read bytes, therefore n not c.n.
+	return n, err
+}
 
 func main() {
+	inp := &counter{Reader: os.Stdin, n: 0}
 	fmt.Println("␌")
-	io.Copy(os.Stdout, os.Stdin)
+	io.Copy(os.Stdout, inp)
+	fmt.Println("␄", inp.n)
 }
