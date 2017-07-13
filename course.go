b71fb16072769bd218962b7dbe3751c832cee448
Pararaum <pararaum@gmail.com>
2017-07-13 17:26:39 +0200

    Synchronising sparks.

--- course.go
+++ course.go
@@ -3,21 +3,24 @@ package main
 import (
 	"fmt"
 	"runtime"
+	"sync"
 )
 
-
 func main() {
+	var wg sync.WaitGroup
 	n := runtime.NumCPU()
 	ch := make(chan int)
 	fmt.Println(n)
-	for i := 0; i < 10; i++ {
-		go func(i int) {
+	for i := 0; i < n; i++ {
+		go func(id int) {
 			for x := range ch {
-				fmt.Println(i, x)
+				fmt.Println(id, x)
 			}
 		}(i)
 	}
-	for j := 0; j < 10; j++ {
+	for j := 0; j < 100; j++ {
 		ch <- j
 	}
+	close(ch)
+	wg.Wait()
 }
