b3b37550850ac63c26f8c1109b50c45e66493a4f
Pararaum <pararaum@gmail.com>
2017-07-14 09:15:49 +0200

    Mutex solves the problem but there is still another race.

--- course.go
+++ course.go
@@ -3,14 +3,19 @@ package main
 import (
 	"fmt"
 	"time"
+	"sync"
 )
 
 func main() {
 	var problem int
+	var mu sync.Mutex
+	
 	for i := 0; i < 5; i++ {
 		go func() {
 			for j := 0; j < 10000; j++ {
+				mu.Lock()
 				problem++
+				mu.Unlock()
 			}
 		}()
 	}
