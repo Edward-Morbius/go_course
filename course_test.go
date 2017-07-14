17e0c2dbb2765d8104ea9baabf7112177ee10b8f
Pararaum <pararaum@gmail.com>
2017-07-14 09:38:32 +0200

    Another test.

--- course_test.go
+++ course_test.go
@@ -9,3 +9,10 @@ func TestAnswer(t *testing.T) {
 		t.Errorf("answer %d != 42", a)
 	}
 }
+
+func TestHalfAnswer(t *testing.T) {
+	a := halfAnswer()
+	if a != 21 {
+		t.Errorf("halfAnswer %d != 21", a)
+	}
+}
