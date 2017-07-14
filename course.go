f6b548b5770d8617e8e15fb09e889df541bfb788
Pararaum <pararaum@gmail.com>
2017-07-14 09:12:59 +0200

    Races, detect with '-race' during build time.

--- course.go
+++ course.go
@@ -2,25 +2,18 @@ package main
 
 import (
 	"fmt"
-	"runtime"
-	"sync"
+	"time"
 )
 
 func main() {
-	var wg sync.WaitGroup
-	n := runtime.NumCPU()
-	ch := make(chan int)
-	fmt.Println(n)
-	for i := 0; i < n; i++ {
-		go func(id int) {
-			for x := range ch {
-				fmt.Println(id, x)
+	var problem int
+	for i := 0; i < 5; i++ {
+		go func() {
+			for j := 0; j < 10000; j++ {
+				problem++
 			}
-		}(i)
+		}()
 	}
-	for j := 0; j < 100; j++ {
-		ch <- j
-	}
-	close(ch)
-	wg.Wait()
+	time.Sleep(time.Second)
+	fmt.Println(problem)
 }
